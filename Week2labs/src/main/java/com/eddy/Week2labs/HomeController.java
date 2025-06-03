6534092c407c18074d8cafbe91ed30100b70eec4
Eddy dashner <eddy.nzoabarinda@amalitech.com>
2025-06-03 10:09:58 +0200

    Spring Mvc welcome app

---
+++ Week2labs/src/main/java/com/eddy/Week2labs/HomeController.java
@@ -0,0 +1,14 @@
+package com.eddy.Week2labs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "welcome";
+
+    }
+}
---
+++ Week2labs/src/test/java/com/eddy/Week2labs/Week2labsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddy.Week2labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2labsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
